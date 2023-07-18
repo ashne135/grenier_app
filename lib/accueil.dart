c107e4b4bdd8ca04cdc581c8f6a9fe83e29f362c
unknown <ashneouedraogo@gmail.com>
2023-07-18 19:07:50 +0200

    first commit

---
+++ lib/accueil.dart
@@ -0,0 +1,10 @@
+// Home screen
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: Implement home screen UI
+    return Container();
+  }
+}
---
+++ lib/connexion.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+// Connection screen
+class ConnectionScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: Implement connection screen UI
+    return Container();
+  }
+}
